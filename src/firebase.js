c530e60fa155d602e6bfae9c6318426ee7f1a96e
User <dummyforsites777@gmail.com>
2025-03-22 02:46:03 +0400

    uploading build bc github coulnt build

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,21 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
-import { getAnalytics } from "firebase/analytics";
 import { getStorage } from 'firebase/storage';
 
+// Load Firebase configuration from environment variables
 const firebaseConfig = {
-    apiKey: "AIzaSyAbRzUzEMT48IuJ7Mkl-wt4WzyFGRE7kJA",
-    authDomain: "adavision2025may.firebaseapp.com",
-    projectId: "adavision2025may",
-    storageBucket: "adavision2025may.firebasestorage.app",
-    messagingSenderId: "539191394630",
-    appId: "1:539191394630:web:80d93070b793cd53496547",
-    measurementId: "G-XL7LVX0M42"
-  };
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+};
 
 const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase services
 export const db = getFirestore(app);
 export const storage = getStorage(app);
+
